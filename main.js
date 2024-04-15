a2df6b4e723efa5cedee298fb232aa8001d8806a
64-Casillas <165535151+64-Casillas@users.noreply.github.com>
2024-04-14 21:01:47 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -22,33 +22,3 @@ $(document).ready(function() {
     $('#testimonios .btn-prev').on('click', prevTestimonio);
   });
   
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const cursoMenu = document.querySelector('#curso-menu');
-    const cursoLinks = cursoMenu.querySelectorAll('.nav-link');
-  
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        const targetId = entry.target.getAttribute('id');
-        const correspondingLink = cursoMenu.querySelector(`a[href="#${targetId}"]`);
-  
-        if (entry.isIntersecting) {
-          cursoLinks.forEach(link => link.classList.remove('active'));
-          correspondingLink.classList.add('active');
-        }
-      });
-    }, { threshold: 0.5 });
-  
-    const cursoSections = document.querySelectorAll('.tab-pane');
-    cursoSections.forEach(section => observer.observe(section));
-  
-    cursoLinks.forEach(link => {
-      link.addEventListener('click', e => {
-        e.preventDefault();
-        const targetId = e.target.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-        targetSection.scrollIntoView({ behavior: 'smooth' });
-      });
-    });
-  });
-  
